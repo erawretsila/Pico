6ef78a7e66e0b93e372f63802043af922cbac033
Alister Ware <alister.ware@ntlworld.com>
2021-02-13 22:08:36 +0000

    prevent lockup if sensor missing or fails to trigger return 0xffffffff if sensor times out

--- SR04-PIO/SR04-PIO.py
+++ SR04-PIO/SR04-PIO.py
@@ -5,20 +5,21 @@ from machine import Pin
 
 @asm_pio(set_init=(PIO.IN_LOW),sideset_init=PIO.OUT_LOW)
 def SR04():
-    label ('start')
     wait(0,pin,0)              #check echo pin is low before starting
     mov(x,null) .side(1)[7]    #set trigger pin high & clear x register
-    nop() .side(0)             #return trigger to low
-    wait(1,pin,0)              #wait for start of echo pulse
+    mov(y,null) .side(0)             #return trigger to low
+    label ('wait')              #wait for start of echo pulse
+    jmp (pin,'countdown')       #start timing when pin goes high
+    jmp (y_dec,'wait')          #loop arround untill count expires
+    jmp ('exit')                #exit
     label('countdown')
     jmp(x_dec,'skip')          #decrement x reg
     label('skip')             
     jmp(x,'exit')              #exit if x 0 #time out
     jmp(pin,'countdown')       #ping still high continue counting
+    label ('exit')
     mov(isr, invert(x))        #invert x & prepare to output
     push()                     #output to Fifo
-    label ('exit')
-    jmp ('start')              #echo has timed out, return to start.
     
 #example usage    
 sm=rp2.StateMachine(1,SR04,freq=500000,set_base=Pin(0),sideset_base=Pin(1))
@@ -27,3 +28,4 @@ sm.active(1)
 while True:
     print(sm.get())
     time.sleep(.1)
+ 
